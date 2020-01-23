48b980f6c0ef9b6f767acd81f9e171c1c3480b26
fonkamloic <fonkamloic@gmail.com>
2020-01-23 23:45:18 +0100

    company

---
+++ maps/src/Company.ts
@@ -0,0 +1,31 @@
+import faker from 'faker';
+
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'red';
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+
+  markerContent(): string {
+    return `
+       <div>
+       <h2>Company name is ${this.companyName}</h2>
+       <h3>catchphrase is ${this.catchPhrase}</h3>
+       </div>
+       `;
+  }
+}
