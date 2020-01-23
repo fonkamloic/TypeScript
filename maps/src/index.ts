7596744d24d3e1f346970e5668634ac32de13f71
fonkamloic <fonkamloic@gmail.com>
2020-01-23 23:45:49 +0100

    index.ts

---
+++ maps/src/index.ts
@@ -0,0 +1,18 @@
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const customMap = new CustomMap('map');
+const user = new User();
+const company = new Company();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
+
+
+// const user = new User();
+// const company = new Company();
+
+// console.log(user);
+// console.log(company);
+
