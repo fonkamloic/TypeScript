b86ee9ed96cedbbb1e23ef1bf6e617501e7b9ec0
fonkamloic <fonkamloic@gmail.com>
2020-02-08 13:49:55 +0100

    sorter

---
+++ sort/src/Sorter.ts
@@ -0,0 +1,24 @@
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
+
+export abstract class Sorter {
+  // constructor(public collection: Sortable) {}
+ abstract  length: number;
+ abstract  compare(lIndex: number , rIndex: number): boolean;
+ abstract swap(lIndex:number, rIndex: number): void;
+
+  sort(): void {
+    const { length } = this;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
+        }
+      }
+    }
+  }
+}
