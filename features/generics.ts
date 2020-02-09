886a214b3e45e3d49f9e3cede055799c28e2b0a2
fonkamloic <fonkamloic@gmail.com>
2020-02-09 12:55:41 +0100

    generics

---
+++ features/generics.ts
@@ -0,0 +1,68 @@
+class ArrayOfNumber { 
+    constructor (public collection: number[]) {}
+
+    get(index: number): number {
+        return this.collection[index];
+    }
+}
+
+
+class ArrayOfStrings{
+    constructor(public collection: string[]){}
+    get(index: number): string { 
+        return this.collection[index];
+    }
+}
+
+class ArrayOfAnything<T>{
+    constructor(public collection: T[]){}
+
+    get(index: number): T{
+        return this.collection[index];
+    }
+}
+
+
+function printStrings(arr: string[]): void{
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+function printNumbers(arr: number[]): void{
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+
+function printAnything<T>(arr: T[]): void {
+    for(let i = 0 ; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+
+// Generic Constraints
+
+class Car {
+    print(){
+        console.log('I am a car');
+    }
+}
+class House{
+    print(){
+        console.log('I am a house');
+    }
+}
+
+
+interface Printable {
+    print() : void;
+}
+
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void{
+    for(let i = 0; i < arr.length; i++){
+        arr[i].print();
+    }
+}
